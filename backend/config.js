d575d518435a5c4e702892f4ace9a19b89219b3d
Meateater02 <dqh.phan@gmail.com>
2024-09-01 00:04:55 +1200

    First commit and connect to mongoDB

---
+++ backend/config.js
@@ -0,0 +1,4 @@
+export const PORT = 5555;
+
+export const mongoDBURL =
+  "mongodb+srv://programmer:beginnerProgrammer@bookstoreproject.q1z0p.mongodb.net/books-collection?retryWrites=true&w=majority&appName=bookStoreProject";
