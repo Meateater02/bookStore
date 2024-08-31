3e9fc51cf04eb1c904027ae77f4c6d84877e4a94
Meateater02 <dqh.phan@gmail.com>
2024-09-01 00:36:12 +1200

    added ignore node_modules files and added post function to mongoDB

---
+++ backend/models/bookModel.js
@@ -0,0 +1,14 @@
+import mongoose from "mongoose";
+
+const bookSchema = mongoose.Schema(
+  {
+    title: { type: String, required: true },
+    author: { type: String, required: true },
+    publishYear: { type: Number, reuqired: true },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export const Book = mongoose.model("Book", bookSchema);
